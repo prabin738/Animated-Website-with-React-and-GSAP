10ccd65e5b0f11b86a2372b923dc6dba3ff8d3bf
prabin738 <prabinsingh750@gmail.com>
2025-09-13 21:28:55 +0545

    Home Hero created

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
 import React from "react";
 import "./index.css";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Agence from "./pages/Agence";
+import Projects from "./pages/Projects";
 
 const App = () => {
-  return <div className="text-6xl text-white">Hello</div>;
+  return (
+    <div className="text-white">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="agence" element={<Agence />} />
+
+        <Route path="projects" element={<Projects />} />
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import Video from "../components/home/Video";
+import HomeHeroText from "../components/home/HomeHeroText";
+import HomeBottomText from "../components/home/HomeBottomText";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <div className="h-screen w-screen fixed">
+        <Video />
+      </div>
+      <div className="h-[120vh] w-screen relative flex flex-col">
+        <HomeHeroText />
+        <HomeBottomText />
+      </div>
+    </div>
+  );
 };
 
 export default Home;
