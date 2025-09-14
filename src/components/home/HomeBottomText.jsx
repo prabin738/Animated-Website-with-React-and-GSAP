a42f9d2c924200b463cdac60376e92c2a1bd051b
prabin738 <prabinsingh750@gmail.com>
2025-09-14 13:02:30 +0545

    Home Bottom Done

--- src/components/home/HomeBottomText.jsx
+++ src/components/home/HomeBottomText.jsx
@@ -1,7 +1,16 @@
-import React from "react";
+import { Link } from "react-router-dom";
 
 const HomeBottomText = () => {
-  return <div>HomeBottomText</div>;
+  return (
+    <div className="font-[font2] flex items-center justify-between gap-2 bg-red-500 ">
+      <Link className="text-[6.5vw] leading-tight border-5 border-white rounded-full px-10 py-1 pt-1 p-0 uppercase">
+        Progects
+      </Link>
+      <Link className="text-[6.5vw] leading-tight border-5 border-white rounded-full px-10 py-1 pt-1 p-0 uppercase">
+        Agence
+      </Link>
+    </div>
+  );
 };
 
 export default HomeBottomText;
